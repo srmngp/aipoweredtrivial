8d877ae1498021b8fc669e00912e236caa7a99fd
srmngp <sr.mngp@gmail.com>
2023-10-11 14:18:56 +0200

    Add bg Add Loader List responses

--- app/[category]/page.jsx
+++ app/[category]/page.jsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useEffect, useState } from "react"
 import { useCompletion } from 'ai/react'
+import Loader from "./Loader"
 
 export default function Page({ params }) {
 
@@ -22,26 +23,38 @@ export default function Page({ params }) {
     )
 
     return (
-        <section>
+        <section className="flex min-h-screen flex-col items-center p-12 ">
 
-            <div>My categorie: {params.category}</div>
+            <p className="text-3xl font-semibold text-center">
+                Questions related to: {params.category}
+            </p>
 
             <button onClick={() => checkAndPublish(params.category)}>Generate questions</button>
 
-            {isLoading && (
-                <p>... Loading</p>
-            )}
+            {isLoading && <Loader />}
+
             <div>
                 {questions.map((question, index) => (
-                    <div key={index} className="p-4">
-                        <h3>{question.title}</h3>
-                        <div className="p-4">
-                            <div className="p-2">
-                                <div>Answer: {question.answer}</div>
+
+                    <div key={index} class="bg-blue w-full p-8 flex justify-center font-sans">
+                        <div class="rounded bg-gray-600 w-64 p-2">
+                            <div class="flex justify-between py-1">
+                                <h3 class="text-sm w-5/6">{question.title}</h3>
+                                <div class="text-sm">{index}/{questions.length}</div>
                             </div>
+                            {question.options.map((option, index) => (
+                                <div key={index} class="text-sm mt-2 text-black">
+                                    <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-gray-300">
+                                        {option}
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
+                    
                 ))}
+
+
             </div>
 
         </section>
--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='bg-gradient-to-b from-slate-900 to-slate-700'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
